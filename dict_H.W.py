65807fe6b8a4e948736f0092971f20ff0f6a3c86
shaurya1964 <76860519+shaurya1964@users.noreply.github.com>
2021-01-03 11:14:25 +0530

    Update dict_H.W.py

--- dict_H.W.py
+++ dict_H.W.py
@@ -1,37 +1,34 @@
-keys = ["home_key", "2nd_home-key", "bike_key", "car_key", "shop_key", "safe-key"]
-
-for key in keys:
-  if key == "safe_key":
-    print("open the safe")
-  else:
-    print("this is not safe key") 
-
-key_bunch = {
-  "home_key" : "key1",
-  "2nd_home_key" : "key2",
-  "bike_key" : "key3",
-  "car_key" : "key4",
-  "shop_key" : "key5",
-  "safe_key" : "key5"
+keys = ["SRK", "Salman", "Amir"]
+names = {
+  "SRK" : 20,
+  "Salman" : 40,
+  "Amir" : 50
 }
-
-print(key_bunch["safe_key"]
-      
-food_item = {
-  1: "pizza",
-  2: "vadapav",
-  3: "burger"
-}
-item = int(input("Enter your order:"))
-print("You ordered:{0}".format(food_item[item]))
-
-for key-label in key_bunch.keys():
+print(names)
+for key_label in names.keys():
   print(key_label)
-
-for key_label in key_bunch.values():
+for key_label in names.values():
   print(key_label)
+names["Akshay"] = 60
+print (names) 
+names["Salman"] = 80
+print(names)
+for key_label, value_label in names.items():
+  print(key_label+" "+ str(value_label))
 
-for key_label,key-val  in key_bunch.items():
-  print("key_label+" "+ key_val)
-
-  roll_numbers = {}
+  
+  ======================
+  
+#  {'SRK': 20, 'Salman': 40, 'Amir': 50}
+#SRK
+#Salman
+#Amir
+#20
+#40
+#50
+#{'SRK': 20, 'Salman': 40, 'Amir': 50, 'Akshay': 60}
+#{'SRK': 20, 'Salman': 80, 'Amir': 50, 'Akshay': 60}
+#SRK 20
+#Salman 80
+#Amir 50
+#Akshay 60
