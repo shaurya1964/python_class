1d77382f49ed80e76e602bbed7f75339997f8e07
shaurya1964 <76860519+shaurya1964@users.noreply.github.com>
2021-01-07 22:54:04 +0530

    Create homework_4_Jan.py

---
+++ homework_4_Jan.py
@@ -0,0 +1,22 @@
+cricket_players = []
+
+cricket_players.append("ajinkya")
+cricket_players.append("virat")
+cricket_players.append("jadeja")
+cricket_players.append("bumarh")
+cricket_players.append("bhuneshwar")
+
+for cricket_player in cricket_players:
+  if cricket_player == "bhuneshwar":
+    print("bhuneshwar found")
+  else:
+    print("not found") 
+    
+    #=========
+ #OUTPUT   
+#not found
+#not found
+#not found
+#not found
+#bhuneshwar found   
+  
